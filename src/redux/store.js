92e6d4ae04ebd8fcba1d1cfdd2404522e158bf0b
Brandon Cooke <bcooke32@gatech.edu>
2019-01-14 11:44:03 -0500

    Bug fix: package only worked if you had the Redux addon for a browser

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,17 +2,26 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
+const REDUX_EXTENSION = '__REDUX_DEVTOOLS_EXTENSION__';
+
 const initialState = {};
 
 const middleware = [thunk]
 
-const store = createStore(
-    rootReducer, 
-    initialState, 
+const hasReduxExtension = window.hasOwnProperty(REDUX_EXTENSION) 
+const composedMiddleware = hasReduxExtension ?
     compose(
         applyMiddleware(...middleware),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    ) :
+    compose(
+        applyMiddleware(...middleware)
     )
-);
+
+    const store = createStore(
+        rootReducer, 
+        initialState, 
+        composedMiddleware
+    );
 
 export default store;
